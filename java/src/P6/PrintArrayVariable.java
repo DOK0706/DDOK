c193c87152e9c2fd571a840cb8ee703c0853d6e8
DOK0706 <slime7985@gmail.com>
2021-09-08 20:49:58 +0900

    클래스/메서드

---
+++ java/src/P6/PrintArrayVariable.java
@@ -0,0 +1,10 @@
+package P6;
+//배열 변수의 값 표시
+public class PrintArrayVariable {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        System.out.println("a = " + a);
+        a = null;
+        System.out.println("a = " + a);
+    }
+}
