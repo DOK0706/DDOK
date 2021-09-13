51ab1b08e6b3a9fc08ca4510f8313394618df8c1
DOK0706 <slime7985@gmail.com>
2021-09-13 15:34:50 +0900

    클래스/메서드

---
+++ java/src/Chap06/Circle.java
@@ -0,0 +1,16 @@
+package Chap06;
+
+public class Circle {
+    private void secret() {
+        System.out.println("쉿! 비밀이다.");
+    }
+
+    protected void findRadius() {
+        System.out.println("반지름이 10.0센티이다.");
+    }
+
+    public void findArea() {
+        System.out.println("넓이는 (π*r*r)이다");
+    }
+
+}
