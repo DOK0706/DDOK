6dedd338798644416e47aa9b49c8ab5fb1cbe948
DOK0706 <slime7985@gmail.com>
2021-09-17 23:48:19 +0900

    클래스/메서드

--- java/src/P7/PrintSeason.java
+++ java/src/P7/PrintSeason.java
@@ -8,14 +8,25 @@ class PrintSeason {
     //--- m월의 계절을 표시 ---//
     static void printScreen(int m) {
         switch (m) {
-            case 3: case 4: case 5:
+            case 3:
+            case 4:
+            case 5:
                 System.out.print("봄");
-            case 6: case 7: case 8:
-                System.out.print("여름"); break;
-            case 9: case 10: case 11:
-                System.out.print("가을"); break;
-            case 12: case 1: case 2:
-                System.out.print("겨울"); break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.print("여름");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.print("가을");
+                break;
+            case 12:
+            case 1:
+            case 2:
+                System.out.print("겨울");
+                break;
         }
     }
 
@@ -26,7 +37,7 @@ class PrintSeason {
         do {
             System.out.print("몇 월입니까(1~12): ");
             month = stdIn.nextInt();
-        }   while (month < 1 || month > 12);
+        } while (month < 1 || month > 12);
 
         System.out.print("해당 월의 계절은 ");
         printScreen(month);
