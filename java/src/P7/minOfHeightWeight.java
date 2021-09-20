e97162b9019da3c2877eb7f719ad88ee65f9cc1e
DOK0706 <slime7985@gmail.com>
2021-09-20 19:50:37 +0900

    클래스/메서드

--- java/src/P7/minOfHeightWeight.java
+++ java/src/P7/minOfHeightWeight.java
@@ -8,8 +8,8 @@ class minOfHeightWeight {
     //--- 배열 a의 최솟값을 구해서 반환 ---//
     static int minOf(int[] a) {
         int min = a[0];
-        for (int i = 1; i<a.length; i++) if(a[i] < min) min = a[i];
-return min;
+        for (int i = 1; i < a.length; i++) if (a[i] < min) min = a[i];
+        return min;
 
     }
 
@@ -23,14 +23,14 @@ return min;
         int[] weight = new int[num];    // 체중용 배열 생성
 
         System.out.println(num + "명의 신장과 체중을 입력하자.");
-        for(int i = 0; i<num; i++) {
+        for (int i = 0; i < num; i++) {
             System.out.print((i + 1) + "번의 신장: ");
-        height[i] = stdIn.nextInt();
-            System.out.print((i+1)+"번의 체중: ");
+            height[i] = stdIn.nextInt();
+            System.out.print((i + 1) + "번의 체중: ");
             weight[i] = stdIn.nextInt();
         }
 
-        System.out.println("가장 키가 작은 사람의 신장: " +minOf(height)+"cm");
-        System.out.println("가장 마른 사람의 신장: " +minOf(weight)+"kg");
+        System.out.println("가장 키가 작은 사람의 신장: " + minOf(height) + "cm");
+        System.out.println("가장 마른 사람의 신장: " + minOf(weight) + "kg");
     }
 }
