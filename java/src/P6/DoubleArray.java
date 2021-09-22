1b32fa38c64f1f5280f680246fc192839ab7bccf
DOK0706 <slime7985@gmail.com>
2021-09-22 22:22:10 +0900

    클래스/메서드

--- java/src/P6/DoubleArray.java
+++ java/src/P6/DoubleArray.java
@@ -4,13 +4,13 @@ package P6;
 
 public class DoubleArray {
     public static void main(String[] args) {
-        double[] a=new double[5];
+        double[] a = new double[5];
 
         // 모든 요소의 값 표시
-        System.out.println("a["+0+"]="+a[0]);
-        System.out.println("a["+1+"]="+a[1]);
-        System.out.println("a["+2+"]="+a[2]);
-        System.out.println("a["+3+"]="+a[3]);
-        System.out.println("a["+4+"]="+a[4]);
+        System.out.println("a[" + 0 + "]=" + a[0]);
+        System.out.println("a[" + 1 + "]=" + a[1]);
+        System.out.println("a[" + 2 + "]=" + a[2]);
+        System.out.println("a[" + 3 + "]=" + a[3]);
+        System.out.println("a[" + 4 + "]=" + a[4]);
     }
 }
