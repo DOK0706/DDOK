59b99914d0e944439a0e20bf42e756dbe5caebf0
DOK0706 <slime7985@gmail.com>
2021-09-22 22:30:34 +0900

    클래스/메서드

--- java/src/P6/IntArray54321.java
+++ java/src/P6/IntArray54321.java
@@ -4,8 +4,8 @@ package P6;
 
 public class IntArray54321 {
     public static void main(String[] args) {
-        int[] a={5,4,3,2,1};    // 배열 선언
+        int[] a = {5, 4, 3, 2, 1};    // 배열 선언
 
-        for (int i=0;i<a.length;i++) System.out.println("a[" + i + "]=" + a[i]);
+        for (int i = 0; i < a.length; i++) System.out.println("a[" + i + "]=" + a[i]);
     }
 }
