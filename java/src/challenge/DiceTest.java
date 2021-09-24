c67c59ddd6187162c926904a2cbd303659f38ba4
DOK0706 <slime7985@gmail.com>
2021-09-24 22:11:28 +0900

    클래스/메서드

---
+++ java/src/challenge/DiceTest.java
@@ -0,0 +1,15 @@
+//package challenge;
+//
+//class Dice {
+//    int face;
+//     roll() {
+//        return Math.random() * 6 + 1;
+//    }
+//}
+//
+//public class DiceTest {
+//    public static void main(String[] args) {
+//        Dice d = new Dice();
+//        System.out.println("주사위의 숫자: " + d.roll());
+//    }
+//}
