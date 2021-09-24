d548eaecd1eb7c0a59ee989a3f9085bce41215a7
DOK0706 <slime7985@gmail.com>
2021-09-24 22:11:34 +0900

    클래스/메서드

--- java/src/challenge/CarTest.java
+++ java/src/challenge/CarTest.java
@@ -1,34 +1,30 @@
-//package challenge;
-//
-//import P8.Car;
-//
-//import java.util.Locale;
-//
-//class Car {
-//    String color;
-//    static private int cars = 0;
-//    static  private int redCars = 0;
-//
-//    public Car(String color) {
-//        this.color = color;
-//        cars++;
-//        if (color.toLowerCase().equals("red")) redCars++;
-//    }
-//
-//    static public int getNumOfCar() {
-//        return cars;
-//    }
-//    static public int getNumOfRedCar() {
-//        return redCars;
-//    }
-//}
-//public class CarTest {
-//    public static void main(String[] args) {
-//        Car c1 = new Car("red");
-//        Car c1 = new Car("blue");
-//        Car c1 = new Car("RED");
-//
-//        System.out.println("자동차 수: %d, 빨간색 자동차 수: %d", Car.getNumOfCar(),Car.getNumOfRedCar());
-//    }
-//
-//}
+package challenge;
+
+class Car {
+    static int Cars = 0;
+    static int redCars = 0;
+    String color;
+
+    public Car(String color) {
+        this.color = color;
+        Cars++;
+        if (color.equals("red") || color.equals("RED")) redCars++;
+    }
+
+    static public int getNumOfCar() {
+        return Cars;
+    }
+    static public int getNumOfRedCar() {
+        return redCars;
+    }
+}
+public class CarTest {
+    public static void main(String[] args) {
+        Car c1 = new Car("red");
+        Car c2 = new Car("blue");
+        Car c3 = new Car("RED");
+
+        System.out.printf("자동차 수: %d, 빨간색 자동차 수: %d", Car.getNumOfCar(),Car.getNumOfRedCar());
+    }
+
+}
