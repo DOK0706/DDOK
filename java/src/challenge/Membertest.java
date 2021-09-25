ccdcb2a9aea9c375d2922c9b40d33ba732b3006f
DOK0706 <slime7985@gmail.com>
2021-09-25 23:46:15 +0900

    클래스/메서드

---
+++ java/src/challenge/Membertest.java
@@ -0,0 +1,16 @@
+package challenge;
+
+class Member {
+    private String name;
+    private String id;
+    private String pw;
+    private int age;
+}
+
+public class Membertest {
+    public static void main(String[] args) {
+        Member a = new Member();
+
+    }
+
+}
