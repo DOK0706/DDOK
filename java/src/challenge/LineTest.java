900fbcdaf33b31c3618b731eae80843144fb8ee7
DOK0706 <slime7985@gmail.com>
2021-09-25 23:46:03 +0900

    클래스/메서드

---
+++ java/src/challenge/LineTest.java
@@ -0,0 +1,25 @@
+//package challenge;
+//
+//class Line {
+//    int Line;
+//
+//    int findLine() {
+//        return x * y / 2;
+//    }
+//
+//    int isSameLine(Triangle t) {
+//        if(a.findLine() == Line()) return 1;
+//        else return 0;
+//    }
+//}
+//
+//public class LineTest {
+//    public static void main(String[] args) {
+//        Line a = new Line(1);
+//        Line b = new Line(1);
+//
+//        System.out.println(a.isSameLine(b));
+//        System.out.println(a==b);
+//    }
+//
+//}
