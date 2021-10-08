445688537ee7dd149d4760b047d19d4462162720
DOK0706 <slime7985@gmail.com>
2021-10-08 22:51:34 +0900

    클래스/메서드

---
+++ java/src/test/Circle111Test.java
@@ -0,0 +1,12 @@
+package test;
+
+
+import Chap06.Circle111;
+
+public class Circle111Test {
+    public static void main(String[] args) {
+        Circle111 c1 = new Circle111(5);
+
+        System.out.println(c1.findArea());
+    }
+}
