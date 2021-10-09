018913a888694892f6c315e6926a56b512ef3197
DOK0706 <slime7985@gmail.com>
2021-10-09 22:57:09 +0900

    클래스/메서드

--- java/src/P6/PrintArray.java
+++ java/src/P6/PrintArray.java
@@ -12,16 +12,16 @@ public class PrintArray {
         int n = stdIn.nextInt();        // 요소 수 입력
         int[] a = new int[n];           // 배열 생성
 
-        for (int i = 0;i<n;i++) {
-            System.out.print("a["+i+"]=" );
+        for (int i = 0; i < n; i++) {
+            System.out.print("a[" + i + "]=");
             a[i] = stdIn.nextInt();
         }
         System.out.print("a={");
-        if(n>=2)
-            for(int i=0;i<n-1;i++)  // 앞에서부터 n-1개 요소 표시
-                System.out.print(a[i]+", ");
-            if(n>=1)
-                System.out.print(a[n-1]);   // 마지막 요소 표시
+        if (n >= 2)
+            for (int i = 0; i < n - 1; i++)  // 앞에서부터 n-1개 요소 표시
+                System.out.print(a[i] + ", ");
+        if (n >= 1)
+            System.out.print(a[n - 1]);   // 마지막 요소 표시
         System.out.print("}");
     }
 }
