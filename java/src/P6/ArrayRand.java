30a8b4145b70df9cab621750abd9f1b706a7c3ef
DOK0706 <slime7985@gmail.com>
2021-10-11 21:46:37 +0900

    클래스/메서드

--- java/src/P6/ArrayRand.java
+++ java/src/P6/ArrayRand.java
@@ -14,10 +14,10 @@ public class ArrayRand {
         int n = stdIn.nextInt();    // 요소 수 읽기
         int[] a = new int[n];       // 배열 생성
 
-        for(int i=0;i<n;i++)
-            a[i] = 1+ rand.nextInt(10);     // 1~10 난수
+        for (int i = 0; i < n; i++)
+            a[i] = 1 + rand.nextInt(10);     // 1~10 난수
 
-        for(int i=0;i<n;i++)
-            System.out.println("a["+i+"] = "+a[i]);
+        for (int i = 0; i < n; i++)
+            System.out.println("a[" + i + "] = " + a[i]);
     }
 }
