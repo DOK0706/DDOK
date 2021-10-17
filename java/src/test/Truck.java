3805aed5d34df26c1c00858f6770c071dad841dd
DOK0706 <slime7985@gmail.com>
2021-10-17 18:18:59 +0900

    클래스/메서드

--- java/src/test/Truck.java
+++ java/src/test/Truck.java
@@ -1,9 +1,10 @@
 package test;
 
-public class Truck extends Car{
+public class Truck extends Car {
     int ton;
+
     @Override
     void show2() {
-        System.out.println(carname+"는 "+ton+"톤을 실을 수 있다.");
+        System.out.println(carname + "는 " + ton + "톤을 실을 수 있다.");
     }
 }
