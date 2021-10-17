a3ceb11b4a2ab241a0fb649a7fd6f20b8189e9cc
DOK0706 <slime7985@gmail.com>
2021-10-17 18:20:02 +0900

    클래스/메서드

--- java/src/test/ProductTest.java
+++ java/src/test/ProductTest.java
@@ -2,17 +2,21 @@ package test;
 
 class Product {
     int code;
-    int count=5;
-    void add (){
-        count=count+1;
+    int count = 5;
+
+    void add() {
+        count = count + 1;
     }
-    void sub (){
-        count=count-1;
+
+    void sub() {
+        count = count - 1;
     }
+
     void show() {
-        System.out.println("현재 재고는 "+count+"입니다.");
+        System.out.println("현재 재고는 " + count + "입니다.");
     }
 }
+
 public class ProductTest {
     public static void main(String[] args) {
         Product p = new Product();
