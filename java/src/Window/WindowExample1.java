1abf15878d6e07ec3e193c082213deb953bd9d4d
DOK0706 <slime7985@gmail.com>
2021-10-23 19:40:28 +0900

    test2

---
+++ java/src/Window/WindowExample1.java
@@ -0,0 +1,18 @@
+package Window;
+
+import java.awt.*;
+import javax.swing.*;
+public class WindowExample1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello Java Program");
+        frame.setLocation(500, 400);
+        frame.setPreferredSize(new Dimension(300, 200));
+        Container contentPane = frame.getContentPane();
+        JLabel label = new JLabel("Hello, Java",
+        SwingConstants.CENTER);
+        contentPane.add(label);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
