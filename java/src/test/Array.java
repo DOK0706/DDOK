11c6c009cadffec87ed9ce42eb3afda4734cf55e
DOK0706 <slime7985@gmail.com>
2021-10-28 20:45:12 +0900

    클래스/메서드

---
+++ java/src/test/Array.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.ArrayList;
+
+public class Array {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add("one");
+        al.add("two");
+        al.add("three");
+    }
+}
