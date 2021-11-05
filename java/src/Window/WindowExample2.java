ac19883551cdec782370eb2dcae5d3e463a76828
DOK0706 <slime7985@gmail.com>
2021-11-05 19:36:44 +0900

    클래스/메서드

--- java/src/Window/WindowExample2.java
+++ java/src/Window/WindowExample2.java
@@ -1,7 +1,10 @@
 package Window;
 
+import javax.security.auth.callback.ConfirmationCallback;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WindowExample2 {
     public static void main(String[] args) {
@@ -15,8 +18,26 @@ public class WindowExample2 {
         contentPane.add(text, BorderLayout.CENTER);
         contentPane.add(button, BorderLayout.EAST);
         contentPane.add(label, BorderLayout.SOUTH);
+        ActionListener listener =
+                new ConfirmButtonActionListener(text, label);
+        button.addActionListener(listener);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+}
+
+class ConfirmButtonActionListener implements ActionListener {
+    JTextField text;
+    JLabel label;
+
+    ConfirmButtonActionListener(JTextField text, JLabel label) {
+        this.text = text;
+        this.label = label;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String name = text.getText();
+        label.setText("Hello, " + name);
     }
 }
